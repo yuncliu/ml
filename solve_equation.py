ca8d962b93a3b0fc0e40a3d62406a31432dd3149
yuncliu <lyc1990@qq.com>
2016-01-14 16:00:20 +0800

    rename y_ to yy, change deviation from tf.reduce_sum(tf.square(y - yy)) to tf.square(y - yy)

--- solve_equation.py
+++ solve_equation.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 """Basic usage of tensorflow
-solve eqation Ax = y
+solve eqation Ax = y with interation method
 A = [1, 2]
     [3, 4]
 y = [10]
@@ -14,18 +14,19 @@ if __name__ == '__main__':
     A = tf.constant( [[1., 2.], [3., 4.]])
     y = tf.constant([[10.], [22.]])
     x = tf.Variable(tf.zeros([2, 1])) # x is unkown, so set it a Variable
-    y_ = tf.matmul(A, x)
+    yy = tf.matmul(A, x)
     """ y is the target value
-        y_ is current value
-        so, less (y_ - y) indicates more accurate x
+        yy is current value
+        so, less (yy - y)^2 indicates more accurate x
     """
 
-    deviation = tf.reduce_sum(tf.square(y - y_))
+    deviation = tf.square(y - yy)
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(deviation)
 
     init = tf.initialize_all_variables()
     sess = tf.Session()
     sess.run(init)
+    """iterate 10000 times """
     for i in range(10000):
         sess.run(train_step)
     print(sess.run(x))
